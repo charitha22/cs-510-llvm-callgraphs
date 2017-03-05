f4486f1baf3ccb98ceb083986c53e339db3b69f2
Charitha Saumya <saumyacharitha@gmail.com>
2017-03-04 23:28:45 -0500

    strcut.f done

--- CallGraphPass.cpp
+++ CallGraphPass.cpp
@@ -13,13 +13,78 @@ namespace {
 	class CallGraphPass : public ModulePass {
 	public:
 		static char ID;
+
+		// this map will store which variables/pointers are pointing to functions
+		std::map<Value*, Function*> pointersToFuncMap_;
+		std::map<Value*, Value*> valuesToPointersMap_;
 	
 		CallGraphPass() : ModulePass(ID) {}
+
+		void handleStores(StoreInst* sinst);
+		void handleLoads(LoadInst* linst);
+		void handleCallSites(Instruction& inst);
 
 		bool runOnModule(Module &m) override;
 		
 	}; 
 } 
+
+void CallGraphPass::handleCallSites(Instruction& inst){
+
+	// manage call sites
+	CallSite cs(&inst);
+
+	if(cs.getInstruction()){
+		Value * called = cs.getCalledValue()->stripPointerCasts();
+
+		if(Function * f = dyn_cast<Function>(called) ){
+			// ignoring intrinsic function
+			if(!f->isIntrinsic()){
+				errs()  << "calls function " << f->getName() << " \n";
+			}
+			
+		}
+		else{
+
+			//check the value in valuesToPointersMap_
+			std::map<Value*, Value*>::iterator it = valuesToPointersMap_.find(called);
+
+			if(it != valuesToPointersMap_.end()){
+				// now get the function this pointer points to
+				std::map<Value*, Function*>::iterator it2 = pointersToFuncMap_.find(it->second);
+
+				if(it2 != pointersToFuncMap_.end()){
+					errs()  << "calls function " << it2->second->getName() << " \n";
+				}
+			}
+
+		}
+	}
+	
+}
+
+void CallGraphPass::handleStores(StoreInst* sinst){
+	Value* valOp = sinst->getValueOperand()->stripPointerCasts();
+					
+	// check if this vlaue operand is a function
+	if(Function* f = dyn_cast<Function>(valOp)){
+
+		Value* pointOp = sinst->getPointerOperand()->stripPointerCasts();
+		pointersToFuncMap_.insert(std::pair<Value*, Function*>(pointOp, f)); 
+
+	}
+				
+}
+
+void CallGraphPass::handleLoads(LoadInst* linst){
+	// from which address this is loading
+	Value* pointerOp = linst->getPointerOperand()->stripPointerCasts();
+
+	// result of the load instruction
+	Value* loadVal = dyn_cast<Value>(linst);
+
+	valuesToPointersMap_.insert(std::pair<Value*, Value*>(loadVal, pointerOp));
+}
 
 bool CallGraphPass::runOnModule(Module& m){
 	//errs() << "Call graph for module : "<< m.getName().str().c_str() << "\n";
@@ -28,7 +93,12 @@ bool CallGraphPass::runOnModule(Module& m){
 	for(Module::iterator it =  m.begin(); it != m.end(); it++){
 		
 		Function& f = *it;
-		errs() << f.getName().str() << " : " ;
+
+		if(f.isIntrinsic()){
+			continue;
+		}
+
+		errs() << f.getName().str() << " : \n" ;
 
 		//iterate over basic blocks
 		for(Function::iterator it1 = f.begin(); it1 != f.end(); it1++){
@@ -39,19 +109,20 @@ bool CallGraphPass::runOnModule(Module& m){
 
 				Instruction & inst = *it2;
 
-				CallSite cs(&inst);
-
-				if(!cs.getInstruction()){
-					continue;
-				}
-				//errs() << "Found a function call" << inst << "\n";
-
-				Value * called = cs.getCalledValue()->stripPointerCasts();
-
-				if(Function * f = dyn_cast<Function>(called)){
-					errs()  << f->getName() << " ";
+				// if this instruction is a store. check if its stores function pointers
+				// if it does store the pointer and function in a map
+				if(StoreInst * sinst = dyn_cast<StoreInst>(&inst)){
+					handleStores(sinst);
 				}
 
+				// if the instruction is a load instruction put the loading pointer
+				// and the value it loads to a map
+				else if(LoadInst* linst = dyn_cast<LoadInst>(&inst)){
+					handleLoads(linst);
+				}
+
+				//manage callsites
+				handleCallSites(inst);
 			}
 
 
---
+++ tests/run_test.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+
+
+def main():
+    clang_path = "/home/chartha/Installs/LLVM/build/bin/clang-4.0" 
+    opt_path = "/home/chartha/Installs/LLVM/build/bin/opt"
+    dis_location = "/home/chartha/Installs/LLVM/build/bin/llvm-dis"
+    so_location = "/home/chartha/Installs/LLVM/build/lib/LLVMCallGraph.so"
+
+    # run clang and pass on all the input c files
+    for i in range(1, len(sys.argv)):
+        #print(sys.argv[i])
+        clang_command = clang_path + " -O0  -g -c -emit-llvm " +sys.argv[i]
+        rc = os.system(clang_command)
+
+        if(rc!=0):
+            print("Error running clang command.")
+            sys.exit(-1)
+
+        # generate the .ll file for human readable bitcode
+        dis_command = dis_location + " " + sys.argv[i].split(".")[0] + ".bc"
+
+        rc = os.system(dis_command)
+
+        if(rc!=0):
+            print("Error running llvm-dis command")
+            sys.exit(-1) 
+
+
+        opt_command = opt_path + " -load " +so_location + " -callgraph < " + sys.argv[i].split(".")[0] + ".bc > /dev/null"
+
+        rc = os.system(opt_command)
+
+        if(rc!=0):
+            print("Error running opt command.")
+            sys.exit(-1)
+
+if __name__ == "__main__":
+    main()
+
+
